70649381bb71f30883a3a095079612276ed7d4c1
Ethan Branchereau <ethan.branchereau@epitech.eu>
2024-03-24 19:28:03 +0100

    New feature : press D to download the map you're playing on.

--- game/main_game.c
+++ game/main_game.c
@@ -299,6 +299,37 @@ void print_player_position(Player *player, int pos_x, int pos_y)
     printf("Player at (%d, %d)\n", pos_x, pos_y);
 }
 
+void download_map()
+{
+    Image mapImage = GenImageColor(MAP_WIDTH * TILE_SIZE, MAP_HEIGHT * TILE_SIZE, RAYWHITE);
+
+    for (int i = 0; i < MAP_HEIGHT; i++) {
+        for (int j = 0; j < MAP_WIDTH; j++) {
+            Rectangle dest = { j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE };
+            switch (map[i][j]) {
+                case GRASS:
+                    ImageDrawRectangleRec(&mapImage, dest, GREEN);
+                    break;
+                case SAND:
+                    ImageDrawRectangleRec(&mapImage, dest, YELLOW);
+                    break;
+                case WATER:
+                    ImageDrawRectangleRec(&mapImage, dest, BLUE);
+                    break;
+                case OBSTACLE_GREEN:
+                    ImageDrawRectangleRec(&mapImage, dest, DARKGREEN);
+                    break;
+                case OBSTACLE_BROWN:
+                    ImageDrawRectangleRec(&mapImage, dest, BROWN);
+                    break;
+            }
+        }
+    }
+    
+    ExportImage(mapImage, "map.png");
+    UnloadImage(mapImage);
+}
+
 int main() {
     srand(time(NULL));
 
@@ -360,6 +391,10 @@ int main() {
             } else {
                 PlaySound(fxWav);
             }
+        }
+
+        if (IsKeyPressed(KEY_D)) {
+            download_map();
         }
 
         move_player(player, overworld, &pos_x, &pos_y);
